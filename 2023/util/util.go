9d481e1b40c3796c9ca490b02f08de37711b1437
Keith Ecker <keithjohn1020@gmail.com>
2022-12-06 19:58:45 -0600

    Day 5

--- 2023/util/util.go
+++ 2023/util/util.go
@@ -17,3 +17,17 @@ func ReadInput(fileName string) []string {
 	inputLines := strings.Split(string(content), "\n")
 	return inputLines
 }
+
+type Stack []string
+
+func (s Stack) Push(v string) Stack {
+	return append(s, v)
+}
+
+func (s Stack) Pop() (Stack, string) {
+	l := len(s)
+	if l == 0 {
+		return s, ""
+	}
+	return s[:l-1], s[l-1]
+}
